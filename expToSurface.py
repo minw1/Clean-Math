de4100c896bca7084749f4abd31dbf137c66205a
CarlSchildkraut <carl.schildkraut@lakesideschool.org>
2019-03-14 09:53:19 -0700

    added hitboxes container and translation; added font size support

--- expToSurface.py
+++ expToSurface.py
@@ -8,23 +8,35 @@ LATEX_biFONT_PATH = st.font_locator("cmu_bi.ttf")
 
 
 
-def makeSmaller(font_size):
-    return round(font_size/SQRT2)
+def makeSmaller(font_size, n):
+    return round(font_size/SQRT2**n)
+
+DEFAULT_FONT_SIZE = 48
 
 class smartSurface:
 
     simpleOps = {'+','-'} #'*' will appear as a cdot
     spacing = 10
 
-    def __init__(self, exp, frac_depth, exp_depth): #depth is number of layers into generation we are. 
+    def __init__(self, exp, frac_depth, script_depth): #depth is number of layers into generation we are. 
     	self.surface = None
-    	font = pygame.freetype.Font(LATEX_FONT_PATH, font_size)
-    	iFont = pygame.freetype.Font(LATEX_iFONT_PATH, font_size)
+    	self.hitboxes = [] #(rect, self.exp)
+    	self.font_size = makeSmaller(DEFAULT_FONT_SIZE, script_depth+max(frac_depth-1,0))
+    	self.font = pygame.freetype.Font(LATEX_FONT_PATH, font_size)
+    	self.iFont = pygame.freetype.Font(LATEX_iFONT_PATH, font_size)
 
     	if exp.op.strRep == "()":
-    	if exp.op.strRep in simpleOps:
-    	if exp.op.strRep == "^":
-    	if exp.op.strRep == "frac":
-    	if exp.op.strRep == "*":
+            
+    	elif exp.op.strRep in simpleOps:
+    	elif exp.op.strRep == "^":
+    	elif exp.op.strRep == "frac":
+    	elif exp.op.strRep == "*":
+
+    def translateHitboxes(self,coordinates):
+        newHitboxes = []
+        for hb in self.hitboxes:
+            rect, expression = hb
+            newHitboxes.append((rect.move(coordinates[0],coordinates[1]), expression))
+        return newHitboxes
 
 
