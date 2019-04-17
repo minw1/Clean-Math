addbbc2c30e3edb453facbedb57e740182187eb2
CarlSchildkraut <carl.schildkraut@lakesideschool.org>
2019-04-17 16:08:10 -0700

    added some support for dynamic typing

--- expression.py
+++ expression.py
@@ -1,32 +1,13 @@
 import operation as opn
 import numpy as np
+import wolframalpha
 
-import copy
-
-ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
-
-binOps = {
-    "+":0,
-    "-":0,
-    "*":1,
-    "/":1,
-    "^":2,
-}
-digits = ["0","1","2","3","4","5","6","7","8","9"]
-
-def intcastable(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-
-
+APP_ID = "8T8YA5-3V337TXULH"
+client = wolframalpha.Client(APP_ID)
 
 class Expression:
     #Initializer for composite expression
-    def __init__(self, op="w", expList=[], parent=None):
+    def __init__(self, op, expList, parens=False, cursor=False, cursor_idx=None):
         '''
         Initializes a composite expression
         Parameters:
@@ -36,211 +17,53 @@ class Expression:
         '''
         self.expList = expList #Stores expressions that this expression comprises
         self.op = op
-        self.parent = parent
-        self.index = 0 if op == "w" else None
+        self.parens = parens
+        self.cursor = cursor
+        self.cursor_idx = cursor_idx
+      
+    #Gets string representation of expression
+    def getString(self):
+        returnString = self.op.makeStr(self.expList)
+        if self.parens:
+            returnString = "(" + returnString + ")"
+        return returnString
 
-    def print_tree(self,depth=0):
-        currentNode = self
-        if currentNode.op in binOps:
-            print( ("    "*depth) + currentNode.op+("_"+str(currentNode.index) if currentNode.op=="w" else "") + " connects ")
-        else:
-            print(("    "*depth) + currentNode.op+("_"+str(currentNode.index) if currentNode.op=="w" else "") )
+    #Evaluates expression
+    def eval(self):
+        expString = self.getString()
+        res = client.query(expString) #Gets result from WolframAlpha
+        return next(res.results).text
 
-        for node in currentNode.expList:
-            node.print_tree(depth+1)
+	#Returns expression with parentheses
+    def addParens(self):
+        self.parens = True
+        return self
+	
+	#Adds cursor and index
+    def addCursor(self, idx):
+        self.cursor = True
+        self.cursor_idx = idx
 
-    def find_working(self):
-        currentNode = self
-        if(currentNode.op == "w"):
-            return currentNode
-        if len(currentNode.expList) > 0:
-            for node in currentNode.expList:
-                fw = node.find_working()
-                if not (fw == None):
-                    return fw
-        return None
+    def __repr__(self):
+        return repr(self.op)+' of ('+') and ('.join([repr(k) for k in self.expList])+')'
 
-    def move_working(self, newPlace, index=0):
-        self.scrub_working()
-        w = Expression("w",[],None)
-        w.index = index
-        newPlace.add_child(w)
+class NoOpExpression(Expression):
+    #Initializer for a no-operator expression
+    def __init__(self, strRep, cursor=False, cursor_idx=None):
+        '''
+        Parameters:
+            self: Object being initialized
+            strRep: String representation of expression.
+            '''
+        self.strRep = strRep
+        self.cursor = cursor
+        self.cursor_idx = cursor_idx
 
-    def add_child(self,node):
-        node.parent = self
-        self.expList += [node]
+	#Adds cursor and index
+    def addCursor(self, idx):
+        self.cursor = True
+        self.cursor_idx = idx
+		
+    def __repr__(self):
+        return self.strRep
 
-    def detach_child(self,op):#will only detach the last node with matching op
-        toDetachIndex = -1
-        for index, node in enumerate(self.expList):
-            if node.op == op: 
-                toDetachIndex = index
-        if not (toDetachIndex == -1):
-            del self.expList[toDetachIndex]
-
-    def scrub_working(self):#removes all w nodes from a tree
-        currentNode = self
-        currentNode.detach_child("w")
-        for node in currentNode.expList:
-            node.scrub_working()
-
-
-    def youngestLPAC(self,node,thisop):#child of youngest lower precedence ancestor
-        currentNode = node
-        while True:
-            if currentNode.parent == None:
-                return currentNode
-            if currentNode.parent.op in binOps:
-                if binOps[currentNode.parent.op] < binOps[thisop]:
-                    return currentNode
-            currentNode = currentNode.parent
-
-    def changeNodeParents(expList, newParent): #may have working moving issues
-        for i in expList:
-            i.parent = newParent
-
-    def backspace(self):
-        workingNode = self.find_working()
-        if workingNode == None:
-            print("something very bad has happened")
-        elif workingNode == self:
-            print("backspace called on an empty expression; expression unchanged")
-        elif intcastable(workingNode.parent.op) and workingNode.index>0: #deal with being at the beginning of a number later
-            if(len(workingNode.parent.op) == 1):
-                workingNode.parent.op = "e"
-            else:
-                workingNode.parent.op = workingNode.parent.op[:workingNode.index-1]+workingNode.parent.op[workingNode.index:]
-            workingNode.index -= 1
-        else:
-            print("that case is not yet implemented")
-
-    def leftmost(node):
-        if len(node.expList) < 2: #can't go left if there is only down
-            return node
-        return leftmost(node.expList[0])
-    def is_leftnode(node):
-        if node.parent == None:
-            return None
-        return True if node.parent.expList.index(node)==0 else False
-    def first_rightward_ancestor(node):
-        if node.parent == None:
-            return None
-        if not is_leftnode(node):
-            return node.parent
-        return first_rightward_ancestor(node.parent)
-                
-    def to_left(self):
-        w = self.find_working()
-        working = w.parent
-        lm = leftmost(working)
-
-        if leftmost(self) == w:
-            return working
-        elif lm == working:
-            return first_rightward_ancestor(working)
-        else:
-            return lm
-
-    def add(self,added):
-        workingNode = self.find_working()
-        if workingNode == None:
-            print("something very bad has happened")
-        elif workingNode.parent == None:
-            if added in binOps:
-                thisop = Expression(added,[],None)
-                first = Expression("e",[],None)
-                second = Expression("e",[],None)
-                w = Expression("w",[],None)
-                second.add_child(w)
-                thisop.add_child(first)
-                thisop.add_child(second)
-                Expression.changeNodeParents(thisop.expList,self)#because self  will 'become' thisop, but self's pointer will remain fixed
-                self.__dict__ = copy.copy(thisop.__dict__)#shalllow copy because we want the pointers to maintain the hierarchy's connections
-            if added in digits:
-                thisdig = Expression(added,[],None)
-                w = Expression("w",[],None)
-                w.index = 1
-                thisdig.add_child(w)
-                thisdig.expList[0].parent = self #self will 'become' thisdig, but self's pointer will remain fixed
-                self.__dict__ = copy.copy(thisdig.__dict__) #shallow copy because we want the pointers to maintain the hierarchy's connections
-        elif intcastable(workingNode.parent.op) and added in digits:
-                number = workingNode.parent.op
-                before, after = number[:workingNode.index],number[workingNode.index:]
-                workingNode.parent.op = before+added+after
-                workingNode.index += 1
-        elif workingNode.parent.op == "e" and added in digits:
-                workingNode.parent.op = added
-                workingNode.index += 1
-        elif added in binOps:
-            parent = workingNode.parent
-            if intcastable(parent.op) and 0<workingNode.index<len(parent.op):
-                print("that case is not yet implemented") #insertion is hard
-            elif workingNode.index == 0:
-                YLPAC = workingNode.youngestLPAC(workingNode,added)
-                treeflip = False
-                if(YLPAC == self):
-                    treeflip = True              
-                if treeflip:
-                    self.scrub_working()
-                    top = Expression(added,[],None)
-                    first = Expression("e",[],None)
-                    w = Expression("w",[],None)
-                    first.add_child(w)
-                    top.add_child(first)
-                    top.add_child(copy.copy(self))
-                    Expression.changeNodeParents(top.expList,self)
-                    self.__dict__ = copy.copy(top.__dict__)
-                else:
-                    YLPAC.scrub_working()
-                    medium = Expression(added,[],None)
-                    first = Expression("e",[],None)
-                    w = Expression("w",[],None)
-                    first.add_child(w)
-                    medium.add_child(first)
-                    medium.add_child(copy.copy(YLPAC))
-                    YLPAC.__dict__= copy.copy(medium.__dict__)
-            elif workingNode.index == 1:
-                YLPAC = workingNode.youngestLPAC(workingNode,added)
-                treeflip = False
-                if(YLPAC == self):
-                    treeflip = True              
-                if treeflip:
-                    self.scrub_working()
-                    top = Expression(added,[],None)
-                    second = Expression("e",[],None)
-                    w = Expression("w",[],None)
-                    second.add_child(w)
-                    top.add_child(copy.copy(self))
-                    top.add_child(second)
-                    Expression.changeNodeParents(top.expList,self)
-                    self.__dict__ = copy.copy(top.__dict__)
-                else:
-                    YLPAC.scrub_working()
-                    medium = Expression(added,[],None)
-                    second = Expression("e",[],None)
-                    w = Expression("w",[],None)
-                    second.add_child(w)
-                    medium.add_child(copy.copy(YLPAC))
-                    medium.add_child(second)
-                    YLPAC.__dict__= copy.copy(medium.__dict__)
-            else:
-                print("i don't think this should happen")
-        else:
-            print("that case is not yet supported")
-
-    
-
-
-e =  Expression()
-e.add("*")
-e.add("3")
-e.add("2")
-e.add("+")
-e.add("^")
-e.add("4")
-thirtytwo = e.expList[0].expList[1]
-e.move_working(thirtytwo,1)
-e.add("1")
-e.print_tree()
-e.backspace()
-e.print_tree()
--- plyParser.py
+++ plyParser.py
@@ -2,6 +2,7 @@
 
 import expression as xp
 import operation as op
+import re
 import expToSurface
 import pygame
 
@@ -10,7 +11,7 @@ def mkstr_plus(opList):
 	if len(opList) != 2:
 		opStr = "ERROR"
 	else:
-		opStr = opList[0].getString + "+" + opList[1].getString
+		opStr = opList[0].getString() + "+" + opList[1].getString()
 	return opStr
 
 def mkstr_minus(opList):
@@ -18,7 +19,7 @@ def mkstr_minus(opList):
 	if len(opList) != 2:
 		opStr = "ERROR"
 	else:
-		opStr = opList[0].getString + "-" + opList[1].getString
+		opStr = opList[0].getString() + "-" + opList[1].getString()
 	return opStr
 	
 def mkstr_astrtimes(opList):
@@ -26,15 +27,15 @@ def mkstr_astrtimes(opList):
 	if len(opList) != 2:
 		opStr = "ERROR"
 	else:
-		opStr = opList[0].getString + "*" + opList[1].getString
+		opStr = opList[0].getString() + "*" + opList[1].getString()
 	return opStr
-	
+
 def mkstr_slashdiv(opList):
 	opStr = ""
 	if len(opList) != 2:
 		opStr = "ERROR"
 	else:
-		opStr = opList[0].getString + "/" + opList[1].getString
+		opStr = opList[0].getString() + "/" + opList[1].getString()
 	return opStr
 	
 def mkstr_caretpow(opList):
@@ -42,9 +43,8 @@ def mkstr_caretpow(opList):
 	if len(opList) != 2:
 		opStr = "ERROR"
 	else:
-		opStr = opList[0].getString + "^" + opList[1].getString
+		opStr = opList[0].getString() + "^" + opList[1].getString()
 	return opStr
-
 
 def mkstr_error(opList):
 	return "ERROR"
@@ -58,9 +58,9 @@ POW_OP = op.Operator('^', mkstr_caretpow)
 ERR_OP = op.Operator("ERROR", mkstr_error)
 
 tokens = (
-    'NAME','NUMBER',
+    'NAME', 'VAR', 'NUMBER',
     '1L1R_OP_L0', '1L1R_OP_L1', '1L1R_OP_R2', 'EQUALS',
-    'LPAREN','RPAREN','LBRACK','RBRACK'
+    'LPAREN','RPAREN','LBRACK','RBRACK', 'NUM_CURSOR', 'VAR_CURSOR'
     )
 
 # Tokens
@@ -83,9 +83,9 @@ def t_1L1R_OP_L0(t):
 
 def t_1L1R_OP_L1(t):
 	r'\*|/'
-	if t.value == '*':
+	if t.value == '*' or t.value == '':
 		t.value = MUL_OP
-	elif t.value = '/':
+	elif t.value == '/':
 		t.value = DIV_OP
 	else:
 		t.value = ERR_OP
@@ -93,11 +93,31 @@ def t_1L1R_OP_L1(t):
 
 def t_1L1R_OP_R2(t):
 	r'\^'
-	if t.value = '^':
+	if t.value == '^':
 		t.value = POW_OP
 	else:
 		t.value = ERR_OP
 	return t
+	
+def t_NUM_CURSOR(t):
+	r'(\d+\|\d*)|(\d*\|\d+)'
+	numString = t.value
+	cursor_idx = numString.index('|')
+	numString = numString.replace('|', '')
+	t.value = xp.NoOpExpression(numString, True, cursor_idx)
+	return t
+	
+def t_VAR_CURSOR(t):
+	r'([a-zA-Z]\|)|(\|[a-zA-Z])'
+	varString = t.value
+	cursor_idx = varString.index('|')
+	varString = varString.replace('|', '')
+	t.value = xp.NoOpExpression(varString, True, cursor_idx)
+	return t
+	
+def t_VAR(t):
+	r'[a-zA-Z]'
+	t.value = xp.NoOpExpression(t.value)
 	
 def t_NUMBER(t):
     r'\d+'
@@ -130,13 +150,6 @@ lexer = lex.lex()
 
 # Parsing rules
 
-precedence = (
-    ('left','PLUS','MINUS'),
-    ('left','TIMES','DIVIDE'),
-    ('right','POWER'),
-    ('right','UMINUS'),
-    )
-
 # dictionary of names
 names = { }
 
@@ -144,26 +157,68 @@ def p_statement_assign(p):
     'statement : NAME EQUALS exp0'
     names[p[1]] = p[3]
 
-resultingExpression = None
-
-def p_statement_expr(p):
+def p_statement_exp0(p):
     'statement : exp0'
     p[0] = p[1]
 
 def p_exp0_exp1(p):
-	'exp0: exp1'
+	'exp0 : exp1'
 	p[0] = p[1]
 
 def p_exp1_exp2(p):
-	'exp1: exp2'
+	'exp1 : exp2'
 	p[0] = p[1]	
 
-def p_expression_number(t):
-    'expression : NUMBER'
-    t[0] = t[1]
+def p_exp2_exp3(p):
+	'exp2 : exp3'
+	p[0] = p[1]	
 
+def p_exp3_parens(p):
+    'exp3 : LPAREN exp0 RPAREN'
+    p[0] = p[2]
+
+def p_exp3_number(p):
+    'exp3 : NUMBER'
+    p[0] = p[1]
+
+def p_exp3_cnumber(p):
+    'exp3 : NUM_CURSOR'
+    p[0] = p[1]
+
+def p_exp3_cvar(p):
+    'exp3 : VAR_CURSOR'
+    p[0] = p[1]
+
+def p_exp3_var(p):
+    'exp3 : VAR'
+    p[0] = p[1]
+   
+def p_empty(p):
+     'empty :'
+     pass
+
+def p_exp3_empty(p):
+    'exp3 : empty'
+    p[0] = xp.NoOpExpression(" ")
+
+def p_exp0_exp0ops(p):
+    'exp0 : exp0 1L1R_OP_L0 exp1'
+    expList = [p[1], p[3]]
+    p[0] = xp.Expression(p[2], expList)
+
+def p_exp1_exp1ops(p):
+    'exp1 : exp1 1L1R_OP_L1 exp2'
+    expList = [p[1], p[3]]
+    p[0] = xp.Expression(p[2], expList)
+
+def p_exp1_exp2ops(p):
+    'exp2 : exp3 1L1R_OP_R2 exp2'
+    expList = [p[1], p[3]]
+    p[0] = xp.Expression(p[2], expList)
+
+#Not sure what this does
 def p_expression_name(t):
-    'expression : NAME'
+    'exp0 : NAME'
     try:
         t[0] = names[t[1]]
     except LookupError:
@@ -180,14 +235,21 @@ def p_error(t):
 import ply.yacc as yacc
 parser = yacc.yacc()
 
+def process_string(inputStr):
+    outputStr = inputStr
+    outputStr = re.sub('(?<=\w|\))(?=\()|(?<=\))(?=\w)|(?<=\d)(?=[a-zA-Z])|(?<=[a-zA-Z])(?=\d)', '*', outputStr)
+    return outputStr
+
 error=False
 def get_exp(inputStr):
     global error
     error=False
-    parser.parse(inputStr)
+    inputStr = process_string(inputStr)
+    resultingExpression = parser.parse(inputStr)
     if error:
         raise ValueError("Expression could not parse correctly.")
     else:
-        string = resultingExpression[1]
-        Surface = expToSurface.smartSurface(string)
-        return Surface.surface
+        return resultingExpression
+        #string = resultingExpression.getString()
+        #Surface = expToSurface.smartSurface(string)
+        #return Surface.surface
